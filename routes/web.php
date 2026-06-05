faddd9d8f2df992d64036f3f1e6a5187da1014dd
Nurulashari <nurulasharim@gmail.com>
2026-06-05 18:14:18 +0700

    menyelesaikan halaman 3 siakad

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HalamanController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HalamanController::class, 'index']);
