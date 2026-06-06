ec5b0b286b096c21d8ea98f2108bf012420e83ed
daniaalfatiani26 <alfatianidania@gmail.com>
2026-06-06 15:05:54 +0700

    integrasi halaman 3

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HalamanController; 
+use App\Http\Controllers\NilaiController;
+use App\Http\Controllers\MahasiswaController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\NilaiController;
 
-// Route untuk menampilkan halaman (Form & Tabel)
+
 Route::get('/nilai', [NilaiController::class, 'index'])->name('nilai.index');
-
-// Route untuk memproses simpan data dari Form ke Database
 Route::post('/nilai', [NilaiController::class, 'store'])->name('nilai.store');
-// Route untuk menghapus data berdasarkan ID
 Route::delete('/nilai/{id}', [NilaiController::class, 'destroy'])->name('nilai.destroy');
-// Route untuk menampilkan halaman edit (mengambil data lama berdasarkan ID)
 Route::get('/nilai/{id}/edit', [NilaiController::class, 'edit'])->name('nilai.edit');
-
-// Route untuk memproses perubahan data ke database
 Route::put('/nilai/{id}', [NilaiController::class, 'update'])->name('nilai.update');
-use App\Http\Controllers\MahasiswaController; // Tambahkan ini di paling atas
+
 
 Route::get('/halaman1', [MahasiswaController::class, 'index']);
+
+
+Route::get('/halaman3', [HalamanController::class, 'index']);
